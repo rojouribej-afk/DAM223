038e96f413fba7d76ee637a802022d285bc436be
rojouribej-afk <rojouribej@gmail.com>
2026-09-17 17:17:49 -0600

    Create logica.js
    Se realizó un archivo de js conectado con html donde la lógica muestra un nombre

---
+++ introJS/logica.js
@@ -0,0 +1,6 @@
+       //Todo lo que este dentro de las etiquetas de scrip se puede codificar con el lenguaje de programacion JavaScript 
+
+        let nombre = prompt("Cual es tu nombre?"); //let es para declarar una variable, prompt es para que el usuario ingrese un valor
+        console.log("Hola " + nombre + "!!"); //console.log es para mostrar un mensaje en la consola del navegador
+        document.write("<h1> Hola " + nombre + "!!</h1>"); //document.write es para mostrar un mensaje en la pagina web, h1 es para mostrar un mensaje en grande
+        
