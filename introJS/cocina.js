44214b2a8065031a535b4c3896e71a56178813f4
rojouribej-afk <rojouribej@gmail.com>
2026-09-23 22:34:19 -0600

    Práctica Coffe 2

--- introJS/cocina.js
+++ introJS/cocina.js
@@ -1,58 +1,52 @@
 let inventarioProductos = [];
 
-//let es para declarar una variable, en este caso indicamos que es un arreglo
+const promociones = [
+    { id: 1, nombre: "Descuento 10% en Cafes", descuento: 0.1 },
+    { id: 2, nombre: "Descuento 20% en Postres", descuento: 0.2 },
+    { id: 3, nombre: "Descuento 30% en Te", descuento: 0.3 }
+];
 
-
-
-// declaramos una variable contsante que nunca cambia, y esta guara el objeto que contiene las funciones
 const Cocina = {
+    agregar: function(id, nombre, precio, categoria, stock) { // agrega productos al inventario
+        const nuevoProducto = {id: id, nombre: nombre, precio: precio, categoria: categoria, stock: stock}; 
+        inventarioProductos.push(nuevoProducto);
+    },
 
-    //creamos una funcion q es una propiedad del objeto Cocina
-    //los parametros son los datos que se necesitan para realizar la accion
-agregar: function(id, nombre, precio) {
-    const nuevoProducto = {id: id, nombre: nombre, precio: precio}; //aqui indicamos que es un objeto y le asignamos los valores de los parametros 
-    inventarioProductos.push(nuevoProducto); //agregamos el objeto al arreglo
-}, //coma para separar las funciones del objeto
+    editar: function(id, nuevoNombre, nuevoPrecio) { // actualiza producto mediante el id
+        const producto = inventarioProductos.find(p => p.id == id); 
+        if (producto) {
+            producto.nombre = nuevoNombre;
+            producto.precio = nuevoPrecio;
+        }
+    },
 
-//actualiza producto mediante el id
-editar: function (id, nuevoNombre, nuevoPrecio) {
-    const producto = inventarioProductos.find(p => p.id == id); //find revisa el array elemento por elemento 
-    if (producto) {         //si encuentra el producto actualiza sus valores
-        producto.nombre = nuevoNombre;
-        producto.precio = nuevoPrecio;
+    eliminar: function(id) { // elimina elementos por el id
+        inventarioProductos = inventarioProductos.filter(p => p.id != id);
+    },
+
+    listar: function() { 
+        return inventarioProductos;
+    },
+
+    obtenerPromociones: function() {
+        return promociones;
+    },
+
+    buscarBaratos: function() {
+        return inventarioProductos.filter(p => p.precio < 40);
+    },
+
+    buscarCaros: function() {
+        return inventarioProductos.filter(p => p.precio >= 40);
+    },
+
+    buscarBebidas: function() {
+        return inventarioProductos.filter(p => p.categoria === "bebida");
+    },
+
+    buscarPostres: function() {
+        return inventarioProductos.filter(p => p.categoria === "postre");
     }
-},
+};
 
-eliminar: function(id) { //elimina elemntos por el id
-    inventarioProductos = inventarioProductos.filter(p=> p.id != id);
-},
-
-listar: function() { //regresa lo que hay guardado en el inventario
-    return inventarioProductos;
- }
-}; //punto y coma para indicar que se cierra el objeto, no es obligatorio pero es buena practica para separar del resto del codigo
-
-module.exports = Cocina; //exportamos el objeto Cocina para poder utilizarlo en otros archivos
-
-
-
-
-
-// como funcina inventarioProductos.find(p => p.id == id);
-
-//esta es la regla de busqueda, revisa cada producto (p) y busca si el id del producto es igual al id que se envio como parametro
-//si encuentra el producto, lo guarda en la variable producto
-//si no encuentra el producto, devuelve undefined
-
-//=> significa: para este producto(p) evalua lo siguiente
-
-//p.id == id : es la condicion de busqueda, se pregunta, el id del producto "p" es igual al id qeu recibi de parametro?
-
-//devualve el objeto completo de lo que haya coincidido
-
-
-// inventarioProductos.filter(p => p.id != id)
-
-// filter crea un nuevo arreglo conservando los elemntos que cumplan con la condicion
-
-//osea, pregutna, si el id es diferente el objeto se queda en la nueva lista, y si es igual se elimina de la nueva lista 
+module.exports = Cocina;
